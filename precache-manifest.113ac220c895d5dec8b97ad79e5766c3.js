b7c0b7f69723d2e917bdf286720113927e6d22b9
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-07-20 20:32:25 +0300

    Updates

---
+++ precache-manifest.113ac220c895d5dec8b97ad79e5766c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b942ead3b04ed4f043bff1e3187a79a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c963d434d85c7622040",
+    "url": "/static/css/main.c2ac8aa0.chunk.css"
+  },
+  {
+    "revision": "e0b421eb544ed95164f1",
+    "url": "/static/js/2.8c2996a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c2996a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c963d434d85c7622040",
+    "url": "/static/js/main.b8c5bec1.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
